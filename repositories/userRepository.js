9d5771840f7eeb9f5ac1fc3118a8f821de011fc0
abdelhay <abdelhay.courses@gmail.com>
2021-09-16 21:27:57 +0200

    add route from verify token & return user date

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -20,8 +20,21 @@ module.exports.InsertUser = async (user) => {
 
 module.exports.FindByEmail = async (user) => {
   try {
-    const User_retrieved = await User.findOne({ email: user.email });
-    return User_retrieved ? User_retrieved : false;
+    const userRetrieved = await User.findOne({ email: user.email });
+    return userRetrieved ? userRetrieved : false;
+  } catch (err) {
+    logger.error("Database Selection failed err: ", err);
+    return false;
+  }
+};
+
+module.exports.FindByID = async (user) => {
+  try {
+    const userRetrieved = await User.findOne(
+      { _id: user._id },
+      { _id: 1, name: 1, email: 1 }
+    );
+    return userRetrieved ? userRetrieved : false;
   } catch (err) {
     logger.error("Database Selection failed err: ", err);
     return false;
